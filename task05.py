ebd7563061da5984eb1089e0504ff4539445a771
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-20 17:02:34 +0500

    Bajarildi

--- task05.py
+++ task05.py
@@ -1,1 +1,15 @@
+# Foydalanuvchi bir nechta xabarlarni `|` bilan ajratib kiritadi. Siz har birini alohida qatorda ko‘rsating.
+# **Masalan:**
 
+# ```text
+# Input: "Salom|Qalesiz?|Yaxshi o‘tdimi bugun?"  
+# Output:  
+# Salom  
+# Qalesiz?  
+# Yaxshi o‘tdimi bugun?
+text = "Salom|Qalesiz?|Yaxshi o‘tdimi bugun?"
+
+result = text.split("|")
+
+for i in result:
+    print(i)
