eac12a08e497ecd5275613f9a2084346ff826704
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-20 17:01:52 +0500

    Bajarildi

--- task03.py
+++ task03.py
@@ -1,1 +1,12 @@
+# SONDAN RAKAMLARNI AJRATISH**
 
+# **Vazifa:** Butun son kiriting. Uning raqamlarini `"-"` belgisi bilan ajratib bering.
+# **Masalan:**
+
+# ```text
+# Input: 24567  
+# Output: "2-4-5-6-7"
+
+num = input("Butun son  kiriting >>")
+
+print("-".join(num))
