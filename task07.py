3b2d2346da3b90c70583df89964f9bae5d5bc033
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-20 17:03:06 +0500

    Bajarildi

--- task07.py
+++ task07.py
@@ -1,1 +1,11 @@
+# MA’LUMOTNI FORMATDAN O‘QISH**
 
+# **Vazifa:** Quyidagi formatda kiritilgan ma’lumotni o‘qib, chiroyli ko‘rinishda chiqarish:
+# `"Ism:Ali, Familiya:Valiyev, Yil:2002"`
+# **Output:**
+
+name  = input("Ismingizni kiriting: ").capitalize()
+last_name = input("Familiyanigzni kiriting: ").capitalize()
+age = input("Yoshingzni kiriting: ")
+
+print(f"Ism:{name}, Familiya:{last_name}, Yil:{age} ")
