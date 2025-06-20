7047328f6932f5ca8affe2d1d22ed6c3568d19a8
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-20 17:02:16 +0500

    Bajarildi

--- task04.py
+++ task04.py
@@ -1,1 +1,16 @@
+#  EMAIL DOMENLARINI AJRATISH**
 
+# **Vazifa:** Foydalanuvchi bir nechta email adreslarini vergul bilan kiritadi. Siz har bir emaildan faqat domen (`@gmail.com`, `@mail.ru` va hokazo) qismini ajratib, takrorlanmas qilib ro‘yxatda qaytaring.
+# **Masalan:**
+
+# ```text
+# Input: "ali@gmail.com,vali@mail.ru,karim@gmail.com"  
+# Output: ['@gmail.com', '@mail.ru']
+# ```
+
+email_adress = input("Email manzillarini vergul bilan kiriting: ")
+email = email_adress.split(",")
+
+
+
+print(email)
