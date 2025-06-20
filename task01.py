e30f33ee0873196e0dd18bacf6ff684cd5c7cf95
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-20 16:59:59 +0500

    Bajarildi

--- task01.py
+++ task01.py
@@ -1,1 +1,13 @@
+# Foydalanuvchi FISH (Ism Familiya Sharif) ni kiritadi. Siz uni `'Familiya, Ism Sharif'` shaklida qaytaring.
+# **Masalan:**
 
+# ```text
+# Input: "Aliyev Vali G'ani o‘g‘li"  
+# Output: "Vali G'ani o‘g‘li, Aliyev"
+# ```
+
+name = input("Ismingizni kiriting>>")
+last_name = input("Faamiliya kiriting>>")
+patronymic = input("Otasini ismi>>")
+
+print(f"{name} {patronymic} ,{last_name} .".title())
